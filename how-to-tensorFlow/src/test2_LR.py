4443123f24af98c7b4cedd608f5f995c5f1f6af2
DanielBasara <danielxu233@gmail.com>
2021-10-10 00:59:45 +0900

    2021年10月10日

---
+++ how-to-tensorFlow/src/test2_LR.py
@@ -0,0 +1,7 @@
+# 线性回归（连续值预测）
+import tensorflow as tf
+
+# gradient Descent
+# 存在多种梯度下降的算法比如：SGD，Momentum，NAG，Adagrad，Rmsprop
+
+#   离散值用 yes or no 用logistic Regression求解，分类用 Classification来求解 连续值用 Regression求解
