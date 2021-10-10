bfebd5da457e67f88247f9b978cba0b70e6aec90
DanielBasara <danielxu233@gmail.com>
2021-10-10 23:33:06 +0900

    增加思维导图提升学习效果，增加多分类问题实例

---
+++ how-to-tensorFlow/src/DLwP_reuters.py
@@ -0,0 +1,51 @@
+import imp
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from keras import layers
+from keras.utils.np_utils import to_categorical
+import matplotlib.pyplot as plt
+
+reuters = keras.datasets.reuters
+
+(datas_train, labels_train), (datas_test,
+                              labels_test) = reuters.load_data(num_words=10000)
+
+
+# %%
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+
+# %%
+x_train = vectorize_sequences(datas_train)
+x_test = vectorize_sequences(datas_test)
+
+y_train = to_categorical(labels_train)
+y_test = to_categorical(labels_test)
+
+# %%
+model = keras.models.Sequential([
+    layers.Dense(256, activation='relu', input_shape=(10000, )),
+    layers.Dense(256, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(46, activation='softmax')
+])
+
+model.compile(optimizer='Adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# %%
+model.summary()
+history = model.fit(x_train,
+                    y_train,
+                    epochs=6,
+                    validation_data=(x_test, y_test))
+
+# %%在新数据上生成预测结果
+predictions = model.predict(x_test)
+np.argmax(predictions[2])
--- how-to-tensorFlow/src/test1_helloTF.py
+++ how-to-tensorFlow/src/test1_helloTF.py
@@ -12,20 +12,25 @@ import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+(datas_train, labels_train), (datas_test, labels_test) = mnist.load_data()
+x_train = tf.convert_to_tensor(datas_train, dtype=tf.float32) / 255
+x_test = tf.convert_to_tensor(datas_test, dtype=tf.float32) / 255
+# x_train, x_test = datas_train / 255.0, datas_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer='adam',
+model.compile(optimizer='SGD',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train,
+          labels_train,
+          epochs=8,
+          validation_data=(datas_test, labels_test))
 
-model.evaluate(x_test,  y_test, verbose=2)
+# model.evaluate(x_test, labels_test, verbose=2)
